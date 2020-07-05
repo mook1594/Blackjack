74c5e6c08582246517b68143b8a4fee3ac6a44a5
kjm <jm.kang@Qualson.com>
2020-07-05 15:37:59 +0900

    test : 카드를 한장 꺼내온다.

--- src/test/java/com/mook1594/blackjack/card/CardDeckTest.java
+++ src/test/java/com/mook1594/blackjack/card/CardDeckTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CardDeckTest {
 
@@ -36,7 +38,7 @@ public class CardDeckTest {
 
     @Test
     @DisplayName("카드를 섞는다.")
-    public void getCard() {
+    public void cardShuffle() {
         CardDeck deck = new CardDeck();
         deck.makeCard();
         List<Card> cards = deck.getCards();
@@ -56,6 +58,27 @@ public class CardDeckTest {
         card1 = cards.get(0);
         assertThat(card.equals(card1)).isEqualTo(false);
         System.out.println(card1);
+    }
+
+    @Test
+    @DisplayName("카드를 가져온다.")
+    public void getCard() {
+        CardDeck deck = new CardDeck();
+        deck.makeCard();
+        Card card = deck.getCard();
+
+        assertThat(card).isNotEqualTo(null);
+        assertThat(deck.getCards().size()).isNotEqualTo(51);
+    }
+
+    @Test
+    @DisplayName("예외: 카드가 없을때 카드를 꺼내온다.")
+    public void getCard1() {
+        CardDeck deck = new CardDeck();
+
+        assertThatThrownBy(() -> {
+            Card card = deck.getCard();
+        }).isInstanceOf(NoSuchElementException.class);
     }
 
 }
