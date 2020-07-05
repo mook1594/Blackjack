7eec83eeb48f588430c93871dca04bb19ce51816
kjm <jm.kang@Qualson.com>
2020-07-05 15:19:24 +0900

    feature: 카드 뭉치를 만든다.

--- src/main/java/com/mook1594/blackjack/card/CardDeck.java
+++ src/main/java/com/mook1594/blackjack/card/CardDeck.java
@@ -1,9 +1,14 @@
 package com.mook1594.blackjack.card;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
     private List<Card> cards;
+
+    public CardDeck() {
+        cards = new ArrayList<>();
+    }
 
     public List<Card> getCards() {
         return cards;
@@ -14,5 +19,11 @@ public class CardDeck {
     }
 
     public void makeCard() {
+        for (int i = 1; i <= 13; i++) {
+            cards.add(new Card(i, CardType.DIAMOND));
+            cards.add(new Card(i, CardType.SPADE));
+            cards.add(new Card(i, CardType.CLUB));
+            cards.add(new Card(i, CardType.HEART));
+        }
     }
 }
