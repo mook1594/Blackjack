74ec5e498bcea16ad93063b93d5431c506018ed9
kjm <jm.kang@Qualson.com>
2020-07-09 18:58:09 +0900

    feature : 카드를 준다

--- src/main/java/com/mook1594/blackjack/player/Dealer.java
+++ src/main/java/com/mook1594/blackjack/player/Dealer.java
@@ -1,4 +1,34 @@
 package com.mook1594.blackjack.player;
 
+import com.mook1594.blackjack.card.Card;
+import com.mook1594.blackjack.card.CardDeck;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealer {
+
+    private List<Card> dealerCards;
+
+    private CardDeck cardDeck;
+
+    public Dealer() {
+        dealerCards = new ArrayList<>();
+    }
+
+    public Dealer(CardDeck cardDeck) {
+        this.cardDeck = cardDeck;
+    }
+
+    public void takeCard(Card card) {
+        dealerCards.add(card);
+    }
+
+    public int getCardCount() {
+        return dealerCards.size();
+    }
+
+    public Card giveCard() {
+        return cardDeck.getCard();
+    }
 }
--- src/test/java/com/mook1594/blackjack/player/DealerTest.java
+++ src/test/java/com/mook1594/blackjack/player/DealerTest.java
@@ -1,9 +1,12 @@
 package com.mook1594.blackjack.player;
 
 import com.mook1594.blackjack.card.Card;
+import com.mook1594.blackjack.card.CardDeck;
 import com.mook1594.blackjack.card.CardType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DealerTest {
 
@@ -14,5 +17,21 @@ public class DealerTest {
 
         Card card = new Card(10, CardType.DIAMOND);
         dealer.takeCard(card);
+
+        assertThat(dealer.getCardCount() > 0).isEqualTo(true);
     }
+
+    @Test
+    @DisplayName("카드를 준다.")
+    public void giveCard() {
+        CardDeck cardDeck = new CardDeck();
+        cardDeck.makeCard();
+
+        Dealer dealer = new Dealer(cardDeck);
+
+        Card card = dealer.giveCard();
+
+        assertThat(card).isNotEqualTo(null);
+    }
+
 }
