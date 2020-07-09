b385dd4ed01ceae5df00bb32e218cefabc825feb
kjm <jm.kang@Qualson.com>
2020-07-09 18:14:44 +0900

    test : 딜러 카드 받기 테스트

--- src/test/java/com/mook1594/blackjack/player/DealerTest.java
+++ src/test/java/com/mook1594/blackjack/player/DealerTest.java
@@ -1,4 +1,18 @@
 package com.mook1594.blackjack.player;
 
+import com.mook1594.blackjack.card.Card;
+import com.mook1594.blackjack.card.CardType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class DealerTest {
+
+    @Test
+    @DisplayName("카드를 받는다.")
+    public void takeCard() {
+        Dealer dealer = new Dealer();
+
+        Card card = new Card(10, CardType.DIAMOND);
+        dealer.takeCard(card);
+    }
 }
