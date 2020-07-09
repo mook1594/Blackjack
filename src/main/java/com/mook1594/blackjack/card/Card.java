02eb566409c0b1c9b2ee7ca100181b0ccdea2632
kjm <jm.kang@Qualson.com>
2020-07-09 18:05:20 +0900

    feature : 카드 점수 얻기

--- src/main/java/com/mook1594/blackjack/card/Card.java
+++ src/main/java/com/mook1594/blackjack/card/Card.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import java.util.Objects;
 
 public class Card {
+    @Getter
     private int number;
 
     @Getter
--- src/main/java/com/mook1594/blackjack/rule/Rule.java
+++ src/main/java/com/mook1594/blackjack/rule/Rule.java
@@ -1,4 +1,16 @@
 package com.mook1594.blackjack.rule;
 
+import com.mook1594.blackjack.card.Card;
+
 public class Rule {
+    public static int getScoreFromCard(Card card) {
+
+        if (card.getNumber() == 1) {
+            return 1;
+        } else if (card.getNumber() > 10) {
+            return 10;
+        }
+
+        return card.getNumber();
+    }
 }
